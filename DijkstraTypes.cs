307ad690ae32d6fadf8427aaeb6b390476504862
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-06-07 13:49:48 +0200

    Réorganiser l'espace de noms Types et ajouter la classe Sommet

--- DijkstraTypes.cs
+++ DijkstraTypes.cs
@@ -1,9 +1,29 @@
 namespace DijkstraCS {
-	namespace Types {
-		public enum TypeLien {
+	namespace Types
+	{
+		public enum TypeLien
+		{
 			LIEN_UNI = 0,
 			LEIN_BI = 2,
 			ERR = 3
+		}
+
+		public class Sommet
+		{
+			private Int64 id;
+			readonly private String name;
+
+			private Double x, y, z;
+
+			public Sommet(Int64 id, String name, Double x, Double y, Double z)
+			{
+				this.id = id;
+				this.name = name;
+
+				this.x = x;
+				this.y = y;
+				this.z = z;
+			}
 		}
 	}
 }
