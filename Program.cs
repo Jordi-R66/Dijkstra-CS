9e29db9144af8ad2f3232356e00ce0d45ae98664
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-06-07 14:45:22 +0200

    Réorganiser la méthode Main pour améliorer la lisibilité du code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using DijkstraCS.Types;
+
 namespace DijkstraCS {
 	internal class Program {
-		static void Main() {
+		static void Main()
+		{
+			Coords test = new(1.0, 2.0, 3.0);
 
+			Console.WriteLine(test);
+			Console.WriteLine(test.getNorm());
 		}
 	}
 }
